0d995ccdb4d6bcdb010a6a641b88aea888ecfd57
Maksim Levental <maksim.levental@gmail.com>
2017-09-17 12:26:19 -0400

    sending emails

--- main.py
+++ main.py
@@ -3,38 +3,59 @@ import ssl
 from urllib.parse import parse_qs, urlsplit
 from oauth2client.client import AccessTokenCredentials
 import httplib2
-from googleapiclient import sample_tools
-
 import sendgrid
-import os
 from sendgrid.helpers.mail import *
 from bs4 import BeautifulSoup
+import environ
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+root = environ.Path(__file__)
+env = environ.Env(DEBUG=(bool, False), )
+environ.Env.read_env()
+
+sg = sendgrid.SendGridAPIClient(apikey=env('SENDGRID_API_KEY'))
 
 
-def send_email(to_email):
-    from_email = Email("test@example.com")
-    # to_email = Email("test@example.com")
-    subject = "Sending with SendGrid is Fun"
-    content = Content("text/plain", "and easy to do anywhere, even with Python")
+def send_email(to_email, from_name):
+    from_email = Email(
+        email=from_name.replace(' ', '.') + '@howdoesitfeeltobeafiction.org')
+    to_email = Email(email=to_email)
+    subject = "How does it feel to be a fiction?"
+    content = Content("text/html", '''This is an invitation to participate in a 
+    performance—of which you are now the 
+    audience.  This performance is textual and takes place via email 
+    dissemination.  To participate in the work, 
+    click <a href="https://www.howdoesitfeeltobeafiction.org/">
+    https://www.howdoesitfeeltobeafiction.org/</a>.  
+    The email address you received this from was generated as a result of this 
+    performance.  
+    It repeats the name of the person who last participated and thus marks the 
+    path of 
+    this work's viral circulation.  
+    If you reply to this email, your message will be routed to the author 
+    of the 
+    performance.''')
     mail = Mail(from_email, subject, to_email, content)
-    response = sg.client.mail.send.post(request_body=mail.get())
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    try:
+        response = sg.client.mail.send.post(request_body=mail.get())
+    except Exception as e:
+        print(mail.get())
+        print(e.body)
+        print(str(e))
+    if env('DEBUG'):
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
 
 
 def get_gmail_contacts(access_token):
-    # credentials = AccessTokenCredentials(
-    #     'ya29.GlvJBN9Alr03TltXRHfRzoLrFPeTmXALk_CwAQWw_dfGwM3_LFT25ILo_orw8aA34OzXVdu1cYnx4vGsFO1Is4Jx2TMtxxTa4yD67w-naa89xjMcma_yCvvdo0So',
-    #     'my-user-agent/1.0')
     credentials = AccessTokenCredentials(access_token, 'my-user-agent/1.0')
     http = httplib2.Http()
     http = credentials.authorize(http)
-    resp, content = http.request('https://www.google.com/m8/feeds/contacts/default/full?max-results=10')
+    resp, content = http.request(
+        'https://www.google.com/m8/feeds/contacts/default/full?max-results=10')
     soup = BeautifulSoup(content, 'html.parser')
-    return map(lambda x: x.attrs['address'], soup.find_all(attrs={'address': True}))
+    return map(lambda x: x.attrs['address'],
+               soup.find_all(attrs={'address': True}))
 
 
 class Handler(SimpleHTTPRequestHandler):
@@ -54,7 +75,9 @@ class Handler(SimpleHTTPRequestHandler):
 
 
 httpd = HTTPServer(('localhost', 1443), Handler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.pem', server_side=True)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.pem',
+                               server_side=True)
 # httpd.serve_forever()
-print(list(get_gmail_contacts(
-    'ya29.GlvJBN9Alr03TltXRHfRzoLrFPeTmXALk_CwAQWw_dfGwM3_LFT25ILo_orw8aA34OzXVdu1cYnx4vGsFO1Is4Jx2TMtxxTa4yD67w-naa89xjMcma_yCvvdo0So')))
+# print(list(get_gmail_contacts(
+#     'ya29.GlvJBN9Alr03TltXRHfRzoLrFPeTmXALk_CwAQWw_dfGwM3_LFT25ILo_orw8aA34OzXVdu1cYnx4vGsFO1Is4Jx2TMtxxTa4yD67w-naa89xjMcma_yCvvdo0So')))
+# send_email('maksim@preview.tech', 'max bob')
