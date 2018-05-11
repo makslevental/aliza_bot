c74b3f5e8df915b86e8459f228a84e53ab088593
max <maksim.levental@gmail.com>
2018-05-11 09:19:29 -0400

    j

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from flask import Flask, request, render_template, redirect, make_response
 from wtforms import Form, TextAreaField, PasswordField, validators
 from wtforms.widgets import TextArea
 from flask_wtf.csrf import CSRFProtect
+import unidecode
+from flask import jsonify
+import json
 
 root = environ.Path(__file__)
 env = environ.Env(DEBUG=(bool, False), )
@@ -20,20 +23,22 @@ app = Flask(__name__)
 app.secret_key = env('SECRET_KEY')
 csrf = CSRFProtect(app)
 
-log = open('log','w')
+log = open('log', 'a')
+
 
 class EssayForm(Form):
     password = PasswordField('password', [
         validators.DataRequired(),
-        validators.Regexp(env('ESSAY_PASSWORD'), 0,'wrong password')
+        validators.Regexp(env('ESSAY_PASSWORD'), 0, 'wrong password')
     ])
     essay = TextAreaField('essay', widget=TextArea())
 
 
-
 def send_email(to_email, from_name):
-    from_email = Email(
-        email=from_name.replace(' ', '.') + '@howdoesitfeeltobeafiction.org')
+
+    from_name = unidecode.unidecode(from_name)
+    to_email = unidecode.unidecode(to_email)
+    from_email = Email(email=from_name.replace(' ', '.') + '@howdoesitfeeltobeafiction.org')
     to_email = Email(email=to_email)
     subject_english = "How does it feel to be a fiction?"
     # subject_spanish = "¿Cómo se siente ser una ficción?"
@@ -51,20 +56,10 @@ def send_email(to_email, from_name):
     If you reply to this email, your message will be routed to the author 
     of the 
     performance.'''
-    body_spanish = '''Esta es una invitación a participar de un performance del cual Ud. es, ahora, la
-    audiencia.  Se trata de un performance textual que ocurre a través de la
-    diseminación en línea de correos electrónicos.  Para participar, visite
-    <a href="https://www.howdoesitfeeltobeafiction.org/">
-    https://www.howdoesitfeeltobeafiction.org/</a>.  La dirección que le envía este correo
-    se generó como resultado de este performance y repite el nombre de la persona
-    que participó más recientemente del proyecto.  Como consecuencia, marca el
-    camino de circulación viral de la obra.  Si Ud. responde a este correo, su
-    mensaje lo recibirá el autor del performance y no la persona cuyo nombre
-    aparece en esta dirección de correo.'''
-    content = Content("text/html", body_english+"<br><br><p> </p>"+body_spanish)
+    footer_english = '''\n\n This piece by Aliza Shvarts is part of the exhibition "Off Scene" on view at Artspace, New Haven until June 30'''
+    content = Content("text/html",
+                      body_english + "<br><br><p> </p>" + footer_english)
     mail = Mail(from_email, subject_english, to_email, content)
-    #if env('DEBUG'):
-    #    log.write(mail.get())
     response = None
     try:
         response = sg.client.mail.send.post(request_body=mail.get())
@@ -72,10 +67,11 @@ def send_email(to_email, from_name):
         log.write(str(mail.get()))
         log.write(str(e.body))
         log.write(str(e))
-    if env('DEBUG') and response:
+    if response:
         log.write(str(response.status_code))
         log.write(str(response.body))
         log.write(str(response.headers))
+    log.flush()
 
 
 def get_gmail_contacts(access_token):
@@ -83,7 +79,7 @@ def get_gmail_contacts(access_token):
     http = httplib2.Http()
     http = credentials.authorize(http)
     resp, content = http.request(
-        'https://www.google.com/m8/feeds/contacts/default/full?max-results=1000')
+            'https://www.google.com/m8/feeds/contacts/default/full?max-results=10000000')
     soup = BeautifulSoup(content, 'html.parser')
     return map(lambda x: x.attrs['address'],
                soup.find_all(attrs={'address': True}))
@@ -94,10 +90,12 @@ def root():
     from_name = request.args.get('from_name')
     access_token = request.args.get('access_token')
     if from_name and access_token:
-        email_contacts = get_gmail_contacts(access_token)
+        email_contacts = list(get_gmail_contacts(access_token))
+        print(email_contacts)
+        log.write(str(email_contacts))
         for email in email_contacts:
             send_email(email, from_name)
-    return make_response("")
+    return make_response("aok")
 
 
 @app.route("/api/change_essay", methods=['POST', 'GET'])
@@ -117,5 +115,4 @@ def change_essay():
 
 @app.route("/api/essay", methods=['GET'])
 def essay_get():
-    return open('essay.html').read()
-
+    return jsonify([open('first_essay.html').read(), open('second_essay.html').read(), open('essay.html').read()])
